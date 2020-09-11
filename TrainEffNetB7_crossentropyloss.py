ee7a4afe4baa98b3d8359f55341a53eb7662e6e7
gigi0107 <w_yupaporn@kkumail.com>
2020-09-11 16:06:39 +0700

    add code training by fold1

--- TrainEffNetB7_crossentropyloss.py
+++ TrainEffNetB7_crossentropyloss.py
@@ -25,13 +25,13 @@ batch_size = 16
 epochs = 200
 
 #Train
-dataframe = pd.read_csv('/home/yupaporn/codes/USAI/traindf.csv')
-base_dir = '/media/tohn/SSD/Image/'
+dataframe = pd.read_csv('/home/yupaporn/codes/USAI/traindf_fold1.csv')
+base_dir = '/media/tohn/SSD/Images/Image1'
 os.chdir(base_dir)
 train_dir = os.path.join(base_dir, 'train')
 
 #validation
-valframe = pd.read_csv( '/home/yupaporn/codes/USAI/validationdf.csv')
+valframe = pd.read_csv( '/home/yupaporn/codes/USAI/validationdf_fold1.csv')
 validation_dir = os.path.join(base_dir, 'validation')
 
 from efficientnet.keras import EfficientNetB7 as Net
@@ -95,9 +95,9 @@ test_generator = test_datagen.flow_from_dataframe(
         color_mode= 'rgb',
         class_mode='categorical')
 
-os.chdir('/media/tohn/SSD/ModelTrainByImage/R1')
+os.chdir('/media/tohn/SSD/ModelTrainByImages/R1_1')
 
-root_logdir = '/media/tohn/SSD/ModelTrainByImage/R1/my_logsB72'
+root_logdir = '/media/tohn/SSD/ModelTrainByImages/R1_1/my_logsB72'
 def get_run_logdir():
     import time
     run_id = time.strftime("run_%Y_%m_%d_%H_%M_%S")
